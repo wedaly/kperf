90174805c1b671c8b977842ab434903f2c6508e5
Wei Fu <weifu@microsoft.com>
2024-01-19 08:05:32 +0000

    scripts: support retry in run_runner.sh
    Signed-off-by: Wei Fu <weifu@microsoft.com>

--- scripts/run_runner.sh
+++ scripts/run_runner.sh
@@ -9,7 +9,27 @@ result_file=/data/${POD_NAMESPACE}-${POD_NAME}-${POD_UID}.json
     --result=${result_file} \
     --raw-data
 
-# TODO(weifu): retry if it's not 409
-set +e
-curl -v -H "Content-Type: application/json" -XPOST -d "@${result_file}" ${TARGET_URL}
-set -e
+while true; do
+  set +e
+  http_code=$(curl -s -o /dev/null -w "%{http_code}" -XPOST -d "@${result_file}" ${TARGET_URL} || "50X")
+  set -e
+
+  case $http_code in
+    201)
+      echo "Uploaded it"
+      exit 0
+      ;;
+    409)
+      echo "Has been uploaded, skip"
+      exit 0;
+      ;;
+    404)
+      echo "Leaking pod? skip"
+      exit 1;
+      ;;
+    *)
+      echo "Need to retry after received http code ${http_code} (or failed to connect)"
+      sleep 5s
+      ;;
+  esac
+done
