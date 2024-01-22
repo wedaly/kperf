36dc1ea8f7c112c7fc2ffb5420c2da31fdbb1a9b
Wei Fu <weifu@microsoft.com>
2024-01-22 07:52:06 +0000

    runnergroup: init result subcommand
    Signed-off-by: Wei Fu <weifu@microsoft.com>

--- cmd/kperf/commands/runnergroup/root.go
+++ cmd/kperf/commands/runnergroup/root.go
@@ -36,15 +36,3 @@ var waitCommand = cli.Command{
 		return fmt.Errorf("wait - not implemented")
 	},
 }
-
-var resultCommand = cli.Command{
-	Name:  "result",
-	Usage: "show the result",
-	Flags: []cli.Flag{},
-	Action: func(cliCtx *cli.Context) error {
-		// 1. Check the progress tracker name
-		// 2. Ensure the jobs finished
-		// 3. Output the result
-		return fmt.Errorf("result - not implemented")
-	},
-}
