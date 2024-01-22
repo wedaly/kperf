8b536b90a0420ea76a288da7a53386e58bcd8834
Wei Fu <weifu@microsoft.com>
2024-01-22 05:50:50 +0000

    *: init rg's delete subcommand
    Signed-off-by: Wei Fu <weifu@microsoft.com>

---
+++ cmd/kperf/commands/runnergroup/delete.go
@@ -0,0 +1,20 @@
+package runnergroup
+
+import (
+	"context"
+
+	"github.com/Azure/kperf/runner"
+
+	"github.com/urfave/cli"
+)
+
+var deleteCommand = cli.Command{
+	Name:      "delete",
+	ShortName: "del",
+	Usage:     "delete runner groups",
+	Action: func(cliCtx *cli.Context) error {
+		kubeCfgPath := cliCtx.GlobalString("kubeconfig")
+
+		return runner.DeleteRunnerGroupServer(context.Background(), kubeCfgPath)
+	},
+}
---
+++ runner/runnergroup_delete.go
@@ -0,0 +1,18 @@
+package runner
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Azure/kperf/helmcli"
+)
+
+// DeleteRunnerGroupServer delete existing long running server.
+func DeleteRunnerGroupServer(_ context.Context, kubeconfigPath string) error {
+	delCli, err := helmcli.NewDeleteCli(kubeconfigPath, runnerGroupReleaseNamespace)
+	if err != nil {
+		return fmt.Errorf("failed to create helm delete client: %w", err)
+	}
+
+	return delCli.Delete(runnerGroupServerReleaseName)
+}
