ac70abab8ffeec5290cd56f34dc3356e9f01f91c
Wei Fu <weifu@microsoft.com>
2024-01-24 07:40:02 +0000

    *: use two releases to deploy virtual nodes
    Some cloud providers will delete unknown or not-ready nodes. If we render both
    nodes and controllers in one helm release, helm won't wait for controller ready
    before creating nodes. The nodes will be deleted by cloud providers.
    
    ```bash
    $ kperf vc nodepool add xxx --nodes=200
    
    ...
    
    6s          Normal   RegisteredNode   node/xxx-26               Node xxx-26 event: Registered Node xxx-26 in Controller
    0s          Normal   DeletingNode     node/xxx-110              Deleting node xxx-110 because it does not exist in the cloud provider
    0s          Normal   DeletingNode     node/xxx-139              Deleting node xxx-139 because it does not exist in the cloud provider
    0s          Normal   DeletingNode     node/xxx-175              Deleting node xxx-175 because it does not exist in the cloud provider
    0s          Normal   DeletingNode     node/xxx-162              Deleting node xxx-162 because it does not exist in the cloud provider
    ```
    
    The helm's post-install or post-upgrade hook can ensure that it won't deploy
    nodes until controllers ready. However, resources created by helm hook aren't
    part of helm release. We need extra step to cleanup nodes resources when we
    delete nodepool's helm release.
    
    Based on this fact, we separate one helm release into two. One is for
    controllers and other one is for nodes.
    
    Signed-off-by: Wei Fu <weifu@microsoft.com>

--- virtualcluster/nodes_delete.go
+++ virtualcluster/nodes_delete.go
@@ -2,17 +2,32 @@ package virtualcluster
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/Azure/kperf/helmcli"
+
+	"helm.sh/helm/v3/pkg/storage/driver"
 )
 
 // DeleteNodepool deletes a node pool with a given name.
 func DeleteNodepool(_ context.Context, kubeconfigPath string, nodepoolName string) error {
+	cfg := defaultNodepoolCfg
+	cfg.name = nodepoolName
+
+	if err := cfg.validate(); err != nil {
+		return err
+	}
+
 	delCli, err := helmcli.NewDeleteCli(kubeconfigPath, virtualnodeReleaseNamespace)
 	if err != nil {
 		return fmt.Errorf("failed to create helm delete client: %w", err)
 	}
 
-	return delCli.Delete(nodepoolName)
+	// delete virtual node controller first
+	err = delCli.Delete(cfg.nodeControllerHelmReleaseName())
+	if err != nil && !errors.Is(err, driver.ErrReleaseNotFound) {
+		return fmt.Errorf("failed to cleanup virtual node controller: %w", err)
+	}
+	return delCli.Delete(cfg.nodeHelmReleaseName())
 }
