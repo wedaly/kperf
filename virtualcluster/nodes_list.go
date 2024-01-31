3720317106b2748c2420a55c26c0a86e19cf946a
Wei Fu <weifu@microsoft.com>
2024-01-31 07:30:55 +0000

    *: nodepool list should ignore controllers
    Signed-off-by: Wei Fu <weifu@microsoft.com>

--- virtualcluster/nodes_list.go
+++ virtualcluster/nodes_list.go
@@ -3,6 +3,7 @@ package virtualcluster
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"helm.sh/helm/v3/pkg/release"
 
@@ -16,6 +17,19 @@ func ListNodepools(_ context.Context, kubeconfigPath string) ([]*release.Release
 		return nil, fmt.Errorf("failed to create helm list client: %w", err)
 	}
 
-	return listCli.List()
+	releases, err := listCli.List()
+	if err != nil {
+		return nil, fmt.Errorf("failed to list nodepool: %w", err)
+	}
 
+	// NOTE: Skip node controllers
+	res := make([]*release.Release, 0, len(releases)/2)
+	for idx := range releases {
+		r := releases[idx]
+		if strings.HasSuffix(r.Name, reservedNodepoolSuffixName) {
+			continue
+		}
+		res = append(res, r)
+	}
+	return res, nil
 }
