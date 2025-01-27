f3219fdfc87e1ae9e6e4c7f24bebfe9b4622ec4e
Will Daly <widaly@microsoft.com>
2025-01-27 12:20:56 -0800

    Wip

--- cilium-test/run-kperf.sh
+++ cilium-test/run-kperf.sh
@@ -3,7 +3,7 @@
 set -xe
 
 # KPERF_IMAGE="telescope.azurecr.io/oss/kperf:v0.1.6"
-KPERF_IMAGE="widalytest.azurecr.io/kperf:widalytest001"
+KPERF_IMAGE="widalytest.azurecr.io/kperf:widalytest002"
 
 ../bin/kperf rg run --runner-image=$KPERF_IMAGE --runnergroup="file://$(pwd)/runnergroup-spec.yaml"
 ../bin/kperf rg result
